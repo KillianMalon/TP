1e014a827011112db43d57a84ed5e69985131af3
KillianMalon <76114286+KillianMalon@users.noreply.github.com>
2020-12-16 15:55:01 +0100

    Create nav.php

---
+++ Componants/nav.php
@@ -0,0 +1,21 @@
+<nav>
+<!--    <a href="/" class="navbar-brand">Accueil</a>-->
+    <ul>
+        <li><a href="/tp/?page=home">Accueil</a></li>
+        <li><a href="/tp/?page=panier">Panier</a></li>
+        <li><a href="/tp/?page=deconnexion">Deconnexion</a></li>
+    </ul>
+</nav>
+<style>
+     nav ul li {
+        display: inline;
+        /*display: flex;*/
+        /* flex-direction: row;*/
+
+
+
+    }
+    nav ul li a{
+        margin-right: 55px;
+    }
+</style>
