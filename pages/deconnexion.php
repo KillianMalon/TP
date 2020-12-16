6bbb90d0795fadb5c9cf1d64d24b64b3c3dbd6b3
KillianMalon <76114286+KillianMalon@users.noreply.github.com>
2020-12-16 22:05:41 +0100

    Update deconnexion.php

--- pages/deconnexion.php
+++ pages/deconnexion.php
@@ -1,5 +1,14 @@
 <?php
 
-echo "Votre panier a été vidé"
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
 
-?>
+
+session_destroy(); ?>
+
+<p>Vous êtes bien déconnecté, votre panier a été vidé.</p>
